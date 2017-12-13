406ac672cbeaf25efd91f0e24120e80fa42eef60
saiken0917 <saiken0917@gmail.com>
2017-12-13 20:54:06 +0800

    add webAssembly calculate

---
+++ 20171213/nutcmd.c
@@ -0,0 +1,16 @@
+#include <webassembly.h>
+
+export int add(int x, int y)
+{
+    return x + y;
+}
+
+export int subtract(int x, int y)
+{
+    return x - y;
+}
+
+export int multiply(int x, int y)
+{
+    return x * y;
+}
